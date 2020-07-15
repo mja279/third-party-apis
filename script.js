c77a7f71e61c883be955248f0db80857af790053
Kira <kira@dogmail.com>
2020-07-15 12:59:33 -0400

    working draft.v5

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover({
+        placement : 'top',
+        trigger : 'hover'
+    });
+});
+
 for( i=0; i < localStorage.length; i++ ) {
     $("#"+localStorage.key(i)+" .description").val(localStorage.getItem(localStorage.key(i)));
 }
@@ -13,12 +20,32 @@ $(".saveBtn").on("click", function() {
     var hour = $(this).parent().attr("id");
     localStorage.setItem(hour, plan)
 });
+
+// When user clicks on Clock icon, it forces page refresh
+
+$(".refresh-icon").on("click", function () {
+    //refreshes the page
+    location.reload(true); 
+    alert('Reloading Page. ');
+});
+
+
+//when user clicks on Calendar2 Check icon, it erases ALL. Maybe play with a "cancel" button
+
+$(".check-icon").on("click", function()  {
+    //Reresh page, then clear ALL
+    location.reload(true);
+    localStorage.clear();
+    alert('Clearing schedule, ready to begin anew.') 
+});
+
+
 // Color-code the past, present, and future blocks 
 function colorSchedule(){
     var currentTimeBlock = moment().hours();
     var clock = $(".time-block")
 
-    for( i = 9; i < (clock.length + 9) ; i++ ) {
+    for(i = 9; i < (clock.length + 9); i++ ) {
         var schedule = $("#hour-"+i);
         schedule.removeClass("past")
         schedule.removeClass("present")
